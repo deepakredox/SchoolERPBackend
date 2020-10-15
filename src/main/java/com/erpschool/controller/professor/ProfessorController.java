ac382d26023c8b2494099cf169499b0782f6bb8e
VikasGupta1993 <35274376+VikasGupta1993@users.noreply.github.com>
2020-10-15 23:11:17 +0530

    Update Professor API

--- src/main/java/com/erpschool/controller/professor/ProfessorController.java
+++ src/main/java/com/erpschool/controller/professor/ProfessorController.java
@@ -1,15 +1,12 @@
 package com.erpschool.controller.professor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erpschool.apiresponse.ResponseObjectXML;
@@ -19,112 +16,37 @@ import com.erpschool.serviceInterface.professor.ProfessorServiceInterface;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/professor")
 public class ProfessorController {
 
 	@Autowired
 	private ProfessorServiceInterface professorServiceInterface;
 
-	ResponseObjectXML<ProfessorDTO> responseObjectXML = new ResponseObjectXML<ProfessorDTO>();
+	
 
 	@PostMapping("/addProfessor")
-	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> addnewProfessor(@RequestBody ProfessorDtls addProfessorDtls) {
+	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> addnewProfessorData(@RequestBody ProfessorDtls addProfessorDtls) {
 		
-		List<ProfessorDTO> professorData = new ArrayList<ProfessorDTO>();
-		
-		try {
-			// Professor Added to database
-			professorServiceInterface.addNewProfessor(addProfessorDtls);
-			
-			// Sending response
-			responseObjectXML.setStatusCode(HttpStatus.OK.value());
-			responseObjectXML.setMessage("Record Added Successfully!!!!!!");
-			responseObjectXML.setData(professorData);
-			return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML, HttpStatus.OK);
-		} catch (Exception e) {
-			responseObjectXML.setStatusCode(HttpStatus.BAD_REQUEST.value());
-			responseObjectXML.setMessage("Record is not added");
-			responseObjectXML.setData(null);
-			return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML,HttpStatus.BAD_REQUEST);
-		}
+		return professorServiceInterface.addNewProfessorData(addProfessorDtls);
 	}
 
 	@PostMapping("/editProfessor")
-	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> editProfessor(@RequestBody ProfessorDtls editProfessorDtls) {
+	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> editProfessorData(@RequestBody ProfessorDtls editProfessorDtls) {
 		
-		List<ProfessorDTO> professorEditData = new ArrayList<ProfessorDTO>();
-		try {
-			// Updating the Professor Details
-			professorServiceInterface.editProfessor(editProfessorDtls);
-			
-			// Sending response
-			responseObjectXML.setStatusCode(HttpStatus.OK.value());
-			responseObjectXML.setMessage("Data Update Successfully");
-			responseObjectXML.setData(professorEditData);
-			return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML,HttpStatus.OK);
-			
-		} catch (Exception e) {
-			responseObjectXML.setStatusCode(HttpStatus.BAD_REQUEST.value());
-			responseObjectXML.setMessage("Error while edit the Professor Data");
-			responseObjectXML.setData(null);
-			return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML,HttpStatus.BAD_REQUEST);
-		}
+		return professorServiceInterface.editProfessorData(editProfessorDtls);
+		
 	}
 
 	@PostMapping("/deleteProfessor")
-	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> deleteProfessor(@RequestBody ProfessorDtls delProfessorDtls) {
+	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> deleteProfessorData(@RequestBody ProfessorDtls delProfessorDtls) {
 		
-		List<ProfessorDTO> professorDelData = new ArrayList<ProfessorDTO>();
-		try {
-			// Delete the Professor Details
-			professorServiceInterface.deleteProfessor(delProfessorDtls);
-			
-			// Sending response
-			  responseObjectXML.setStatusCode(HttpStatus.OK.value());
-			  responseObjectXML.setMessage("Data delete Successfully");
-			  responseObjectXML.setData(professorDelData);
-			  return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML,HttpStatus.OK);
-			  
-		} catch (Exception e) {
-			responseObjectXML.setStatusCode(HttpStatus.BAD_REQUEST.value());
-			responseObjectXML.setMessage("Error while deleting the data");
-			responseObjectXML.setData(null);
-			return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML,HttpStatus.BAD_REQUEST);
-		}
+		return professorServiceInterface.deleteProfessorData(delProfessorDtls);	
 	}
 
 	@GetMapping("/getAllProfessor")
-	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> getAllProfessor() {
+	public ResponseEntity<ResponseObjectXML<ProfessorDTO>> getAllProfessorData() {
 
-		List<ProfessorDTO> professorDataList = new ArrayList<ProfessorDTO>();
-
-		try {
-			Iterable<ProfessorDTO> professorDTO = professorServiceInterface.getAllProfessor();
-
-			for (ProfessorDTO professorData : professorDTO) {
-				professorDataList.add(professorData);
-			}
-			
-			System.out.println("Size"+professorDataList.size());
-			
-			if (professorDataList.size() == 0) {
-				responseObjectXML.setStatusCode(HttpStatus.NOT_FOUND.value());
-				responseObjectXML.setMessage("No records found");
-				responseObjectXML.setData(professorDataList);
-				return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML, HttpStatus.NOT_FOUND);
-			}
-			else
-			{
-				responseObjectXML.setStatusCode(HttpStatus.OK.value());
-				responseObjectXML.setMessage("Data Fetch Successfully");
-				responseObjectXML.setData(professorDataList);
-				return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML, HttpStatus.OK);
-			}
-			
-		} catch (Exception e) {
-			responseObjectXML.setStatusCode(HttpStatus.BAD_REQUEST.value());
-			responseObjectXML.setMessage("Error while deleting the data");
-			responseObjectXML.setData(null);
-			return new ResponseEntity<ResponseObjectXML<ProfessorDTO>>(responseObjectXML,HttpStatus.BAD_REQUEST);
-		}
+		return professorServiceInterface.getAllProfessorData();
 	}
 }
+
--- src/main/java/com/erpschool/serviceInterface/professor/ProfessorServiceInterface.java
+++ src/main/java/com/erpschool/serviceInterface/professor/ProfessorServiceInterface.java
@@ -1,17 +1,20 @@
 package com.erpschool.serviceInterface.professor;
 
+import org.springframework.http.ResponseEntity;
+
+import com.erpschool.apiresponse.ResponseObjectXML;
 import com.erpschool.dto.professor.ProfessorDTO;
 import com.erpschool.model.professor.ProfessorDtls;
 
 public interface ProfessorServiceInterface {
 
-	void addNewProfessor(ProfessorDtls addProfessorDtls);
+	ResponseEntity<ResponseObjectXML<ProfessorDTO>> addNewProfessorData(ProfessorDtls addProfessorDtls);
 
-	void editProfessor(ProfessorDtls editProfessorDtls);
+	ResponseEntity<ResponseObjectXML<ProfessorDTO>> editProfessorData(ProfessorDtls editProfessorDtls);
 
-	void deleteProfessor(ProfessorDtls delProfessorDtls);
+	ResponseEntity<ResponseObjectXML<ProfessorDTO>> deleteProfessorData(ProfessorDtls delProfessorDtls);
 
-	Iterable<ProfessorDTO> getAllProfessor();
+	ResponseEntity<ResponseObjectXML<ProfessorDTO>> getAllProfessorData();
 }
 
 
