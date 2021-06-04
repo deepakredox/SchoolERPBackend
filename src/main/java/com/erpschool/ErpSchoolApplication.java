559be4916f2f8895a90265edf682bdf92d603d43
VikasGupta1993 <35274376+VikasGupta1993@users.noreply.github.com>
2021-06-04 22:40:51 +0530

    JWT Expire token

--- src/main/java/com/erpschool/ErpSchoolApplication.java
+++ src/main/java/com/erpschool/ErpSchoolApplication.java
@@ -2,12 +2,26 @@ package com.erpschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ErpSchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErpSchoolApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer()
+	{
+	  return new WebMvcConfigurer() {
+		  public void addCorsMappings(CorsRegistry registry) {
+			  registry.addMapping("/*").allowedHeaders("*").allowedOrigins("*").allowedMethods("*")
+			  .allowCredentials(true);
+		  }
+	  };	
 	}
 
 }
